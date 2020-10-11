bec43c3edb96b537d391f4ada967f1a03762038f
Justin Kim <justinkim7395@gmail.com>
2020-10-10 22:38:08 -0700

    Switch to Implicit Grant Flow for security reasons

--- server.js
+++ server.js
@@ -1,58 +1,56 @@
-let express = require("express");
-let request = require("request");
-let querystring = require("querystring");
-let path = require("path");
-require("dotenv").config();
+// Due to security concerns with storing access tokens I've switched to using an implicit grant flow for now, with possibility of changing later on
+// This was originally Authorization Code Flow and it works pretty well
 
-const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
-const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
-const REDIRECT_URI =
-  process.env.REDIRECT_URI || "http://localhost:8888/callback";
-const FRONTEND_URI = process.env.FRONTEND_URI || "http://localhost:3000";
+// let express = require("express");
+// let request = require("request");
+// let querystring = require("querystring");
+// let path = require("path");
+// require("dotenv").config();
 
-const app = express();
+// const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+// const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+// const REDIRECT_URI = process.env.REDIRECT_URI || "http://localhost:8888/callback";
+// const FRONTEND_URI = process.env.FRONTEND_URI || "http://localhost:3000";
 
-app.use(express.static(path.join(__dirname, "build")));
+// const app = express();
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/login", function (req, res) {
-  res.redirect(
-    "https://accounts.spotify.com/authorize?" +
-      querystring.stringify({
-        response_type: "code",
-        client_id: SPOTIFY_CLIENT_ID,
-        scope: "user-read-private user-read-email",
-        redirect_uri: REDIRECT_URI,
-      })
-  );
-});
+// app.get("/", function (req, res) {
+//     res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
-app.get("/callback", function (req, res) {
-  let code = req.query.code || null;
-  let authOptions = {
-    url: "https://accounts.spotify.com/api/token",
-    form: {
-      code: code,
-      redirect_uri: REDIRECT_URI,
-      grant_type: "authorization_code",
-    },
-    headers: {
-      Authorization:
-        "Basic " +
-        new Buffer(SPOTIFY_CLIENT_ID + ":" + SPOTIFY_CLIENT_SECRET).toString(
-          "base64"
-        ),
-    },
-    json: true,
-  };
-  request.post(authOptions, function (error, response, body) {
-    var access_token = body.access_token;
-    let uri = FRONTEND_URI;
-    res.redirect(uri + "?access_token=" + access_token);
-  });
-});
+// app.get("/login", function (req, res) {
+//     res.redirect(
+//         "https://accounts.spotify.com/authorize?" +
+//             querystring.stringify({
+//                 response_type: "code",
+//                 client_id: SPOTIFY_CLIENT_ID,
+//                 scope: "user-read-private user-read-email",
+//                 redirect_uri: REDIRECT_URI,
+//             })
+//     );
+// });
 
-app.listen(process.env.PORT || 8888);
+// app.get("/callback", function (req, res) {
+//     let code = req.query.code || null;
+//     let authOptions = {
+//         url: "https://accounts.spotify.com/api/token",
+//         form: {
+//             code: code,
+//             redirect_uri: REDIRECT_URI,
+//             grant_type: "authorization_code",
+//         },
+//         headers: {
+//             Authorization: "Basic " + new Buffer(SPOTIFY_CLIENT_ID + ":" + SPOTIFY_CLIENT_SECRET).toString("base64"),
+//         },
+//         json: true,
+//     };
+
+//     request.post(authOptions, function (error, response, body) {
+//         let uri = FRONTEND_URI;
+//         res.redirect(uri);
+//     });
+// });
+
+// app.listen(process.env.PORT || 8888);
--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function App() {
-  return <div className="App">justin</div>;
+    return (
+        <div className="App">
+            <a
+                href={`https://accounts.spotify.com/authorize?response_type=token&client_id=${encodeURIComponent(
+                    process.env.REACT_APP_SPOTIFY_CLIENT_ID
+                )}&redirect_uri=http://localhost:3000`}
+            >
+                Button
+            </a>
+        </div>
+    );
 }
 
 export default App;
